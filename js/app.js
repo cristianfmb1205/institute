e965c8c5800967e233ca05ee33f4f9a773d2bd32
cris <cmosqbone1@gmail.com>
2020-11-30 21:17:00 +0100

    finish the first part of hero nav and hamburger interaction

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.main-nav');
+
+
+hamburger.addEventListener('click', () => {
+
+    nav.classList.toggle('interactive-nav')
+
+
+});
