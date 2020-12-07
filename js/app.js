645c925537cd383e0d3c162c7d88c356e4eeeda7
cris <cmosqbone1@gmail.com>
2020-12-07 19:26:13 +0100

    finish the page with javascript

--- js/app.js
+++ js/app.js
@@ -2,9 +2,40 @@ const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('.main-nav');
 
 
+
+
+
 hamburger.addEventListener('click', () => {
 
     nav.classList.toggle('interactive-nav')
 
 
 });
+
+
+window.addEventListener('scroll', () => {
+
+
+    const card = document.querySelectorAll('#card');
+
+    const scrollTop = document.documentElement.scrollTop;
+
+
+    console.log(scrollTop)
+
+
+
+
+    if (scrollTop > 303) {
+
+
+        card.forEach(element => {
+
+            element.classList.add('card-animation')
+
+        });
+
+    }
+
+
+})
